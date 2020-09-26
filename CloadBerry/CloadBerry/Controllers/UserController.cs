a29bf62206456434759ae790c158edca20006028
ömer şahinler <omerrr_97@hotmail.com>
2020-09-26 20:38:24 +0300

    Controler Atributtes ve Filter oluşturuldu.

---
+++ CloadBerry/CloadBerry/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using CloadBerry.Attributes;
+using CloadBerry.Models;
+using CloadBerry.Models.DTO;
+using CloadBerry.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloadBerry.Controllers
+{
+    [Route("user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
+
+        {
+            _userService = userService;
+        }
+        [SkipAuth]
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDTO login)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var result = await _userService.Login(login);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                else
+                    return Ok(result);
+
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+
+        }
+       
+    }
+}
---
+++ CloadBerry/CloadBerry/Filter/AuthorizationFilter.cs
@@ -0,0 +1,41 @@
+﻿using CloadBerry.Attributes;
+using CloadBerry.Services;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloadBerry.Filters
+{
+    public class AuthorizationFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context,
+                                         ActionExecutionDelegate next)
+        {
+            if (context.ActionDescriptor
+                 .FilterDescriptors
+                 .Any(x => x.Filter is SkipAuthAttribute))
+            {
+                await next();
+                return;
+            }
+
+            var userService = (UserService)context.HttpContext.RequestServices.GetService(typeof(UserService));
+            var authHeader = context.HttpContext.Request.Headers["Authorization"].FirstOrDefault();
+
+            if (authHeader != null)
+            {
+                var currentUser = userService.ValidateToken(authHeader);
+
+                if (currentUser != null)
+                {
+                    await next();
+                    return;
+                }
+            }
+
+            context.HttpContext.Response.StatusCode = 401;
+        }
+    }
+}
